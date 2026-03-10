1297806853f0c426ebbbbf7f0066be9e98faa501
jdavidjha <jdavidjha@gmail.com>
2026-03-10 20:44:42 +0530

    day1

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react"
 
 export default function App(){
-
-  const[age,setAge] = useState(20);
-  const[name, setName] = useState("Nitish");
-  const[count,setCount] = useState(0);
+  const[msg,setmsg] = useState(false);
   return(
     <>
     <h1>Hello</h1>
-    <p>My age is {age}</p>
-    <p>My name is {name}</p>
+    <button onClick={()=>setmsg(!msg)}>Click me</button>
+    {msg && (
+      <>
+      <h1>Hello everyone,
+My name is David, I'm from gwalior (Madhya Pradesh) and I’m a first-year BSc student.
+This is one of the first times I’m presenting my project, so please consider this my debug mode 😄
 
-    <h2>Count:{count}</h2>
-    <button onClick={()=>setCount(count => count+1)}>Increase</button>
+
+First of all, thank you to SkillCaptain for giving me this opportunity.
+
+I am very fond of gaming, and instead of just playing games, I decided to build one myself.
+So today, I’m presenting my project — a Chess Game built using HTML, CSS, and JavaScript.
+
+I focused on keeping the game simple, clear, and user-friendly.
+
+While building this project, I realized something important:
+coding is harder than actual chess
+
+This project is inspired by a nation that produced legends like Viswanathan Anand,
+and it helped me improve my logic, problem-solving skills, and confidence.
+
+Thank you everyone for your time. I hope you enjoyed my project.</h1>
+<p>Thank you</p>
+      </>
+      
+    )}
     </>
   )
 }
