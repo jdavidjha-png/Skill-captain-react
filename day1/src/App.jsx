3e8e9a97cf6988752020b9c103539104e42933f3
jdavidjha <jdavidjha@gmail.com>
2026-03-11 20:45:16 +0530

    day1

--- day1/src/App.jsx
+++ day1/src/App.jsx
@@ -1,36 +1,17 @@
-import { useState } from "react"
-
-export default function App(){
-  const[msg,setmsg] = useState(false);
-  return(
-    <>
-    <h1>Hello</h1>
-    <button onClick={()=>setmsg(!msg)}>Click me</button>
-    {msg && (
-      <>
-      <h1>Hello everyone,
-My name is David, I'm from gwalior (Madhya Pradesh) and I’m a first-year BSc student.
-This is one of the first times I’m presenting my project, so please consider this my debug mode 😄
-
-
-First of all, thank you to SkillCaptain for giving me this opportunity.
-
-I am very fond of gaming, and instead of just playing games, I decided to build one myself.
-So today, I’m presenting my project — a Chess Game built using HTML, CSS, and JavaScript.
-
-I focused on keeping the game simple, clear, and user-friendly.
-
-While building this project, I realized something important:
-coding is harder than actual chess
-
-This project is inspired by a nation that produced legends like Viswanathan Anand,
-and it helped me improve my logic, problem-solving skills, and confidence.
-
-Thank you everyone for your time. I hope you enjoyed my project.</h1>
-<p>Thank you</p>
-      </>
-      
-    )}
-    </>
-  )
+export default function(){
+const list= ["apple, banana. guava, pineapple, litchi"]
+const btn= [1,2,3,4,5,6,7,8,9]
+return(
+  <>
+  <h1>Day6</h1>
+  <ul>
+    {list.map((item,index)=>(
+        <li>{item}</li>
+      ))}
+  </ul>
+  {btn.map((item,index)=>(
+      <button>{item}</button>
+    ))}
+  </>
+)
 }
